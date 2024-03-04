260e231ea0004dcc9241e85a5385b9883116735b
akshay41 <akshujoshi41@gmail.com>
2024-03-04 23:31:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('You are  in AWS  Cloud. this is a demo project for loadbalancer'); //write a response to the client
+  res.write('You are  in AWS  Cloud. this is a demo project for load-balancer'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 4000

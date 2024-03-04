0159b465652a66a4931f4085c301735ca1197177
akshay41 <akshujoshi41@gmail.com>
2024-03-04 23:45:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('We are  in AWS  Cloud. this is a demo project for load-balancer for ric'); //write a response to the client
   res.end(); //end the response
-}).listen(3000); //the server object listens on port 4000
+}).listen(5000); //the server object listens on port 4000
